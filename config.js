0915b5804019af7a0e19e0a39176a8849239aa44
Denny Huang <denny0223@gmail.com>
2019-06-04 00:25:06 +0800

    Add schedule json generator config

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.spreadsheetKey = "19HGiUnPEcDTCK39GWgZlCA2t9F4tpFYe8Zxdd3Bj3PA"
+
+config.default_avatar = "https://coscup.org/2018-assets/img/speakers/coscup.jpg"
+config.avatar_base_url = "https://coscup.org/2019-assets/img/speakers/"
+
+module.exports = config
