ce13b66893a36b7d717d40f533eee605a5586e44
Denny Huang <denny0223@gmail.com>
2019-07-25 22:46:46 +0800

    Change speaker default avatar url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.spreadsheetKey = "19HGiUnPEcDTCK39GWgZlCA2t9F4tpFYe8Zxdd3Bj3PA"
 
-config.default_avatar = "https://coscup.org/2018-assets/img/speakers/coscup.jpg"
+config.default_avatar = "https://coscup.org/2019/avatar.jpg"
 config.avatar_base_url = "https://coscup.org/2019-assets/img/speakers/"
 
 module.exports = config
